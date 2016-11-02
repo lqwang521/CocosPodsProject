a03b38eee9688143ce820b2654b20b32722d197f
ApplePen <731696053@qq.com>
2016-11-02 19:33:25 +0800

    creat

---
+++ CocosPodsProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocosPodsProject
+//
+//  Created by wlq on 2016/11/2.
+//  Copyright © 2016年 htmitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocosPodsProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocosPodsProject
+//
+//  Created by wlq on 2016/11/2.
+//  Copyright © 2016年 htmitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
