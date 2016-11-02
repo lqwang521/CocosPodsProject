fd27c99d8d7a0069f75703ef2d1792c0902c7c5f
ApplePen <731696053@qq.com>
2016-11-02 19:41:35 +0800

    数据库加密
    导入数据库加密需要的库

---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'CocosPodsProject'
+pod 'FMDB', '~> 2.6.2'
+pod 'FMDB/SQLCipher', '~> 2.6.2'
---
+++ Pods/Target Support Files/Pods-CocosPodsProject/Pods-CocosPodsProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocosPodsProject : NSObject
+@end
+@implementation PodsDummy_Pods_CocosPodsProject
+@end
